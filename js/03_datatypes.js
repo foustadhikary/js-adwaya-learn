334c149b91ce4de0da894bac38b863aa146331a9
foustadhikary <134725133+foustadhikary@users.noreply.github.com>
2024-05-18 08:29:02 +0000

    config changes

---
+++ js/03_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"//treat all code as newer version
+// alert("hello") we are using node js not browser
+
+
+console.log(3+3); console.log("adwaya")//code is not readable user readabilty is 0
+
+console.log(4
+    +
+    9
+)// not indentable code readability should be high
+
+let name="adwaya"
+let age=24
+let Islogged="yes"
+
+// number 2 to the power 53
+// bigint
+// string=> always use double quotes""
+// boolean true/false
