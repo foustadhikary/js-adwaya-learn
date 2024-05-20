ebaa375ae45d2aea3a77ea325e34f2ee8246ce9b
foustadhikary <134725133+foustadhikary@users.noreply.github.com>
2024-05-20 04:55:11 +0000

    config changes

--- js/04_conversionOperation.js
+++ js/04_conversionOperation.js
@@ -1,27 +1,58 @@
-// let score=33
+// // let score=33
 
-// // const {score}=req.body we dont have any idea which type of value came as input from the server
-// let sore=undefined
-// console.log(typeof score)
-// console.log(typeof (sore))
+// // // const {score}=req.body we dont have any idea which type of value came as input from the server
+// // let sore=undefined
+// // console.log(typeof score)
+// // console.log(typeof (sore))
 
-// let isNumber=Number(sore)// sore is a string variable is getting converted in number
-// console.log(typeof (isNumber))
-// console.log(isNumber)// it is not a number datatype but when seen the type it display as a number i9f null it shows 0
-// // 33-> number
-// // 33abc->NaN
-// // null->0
-// // undefined->NaN
+// // let isNumber=Number(sore)// sore is a string variable is getting converted in number
+// // console.log(typeof (isNumber))
+// // console.log(isNumber)// it is not a number datatype but when seen the type it display as a number i9f null it shows 0
+// // // 33-> number
+// // // 33abc->NaN
+// // // null->0
+// // // undefined->NaN
 
-// let isLoggedIn=3
-// let BooleanIsLoggedIn=Boolean (isLoggedIn)
-// console.log(typeof BooleanIsLoggedIn)
-// console.log("the value of IsLoggedin is"+BooleanIsLoggedIn)
-// // 1->true
-// // 0->false
-// // "" empty string false
-// // "abc"->true
+// // let isLoggedIn=3
+// // let BooleanIsLoggedIn=Boolean (isLoggedIn)
+// // console.log(typeof BooleanIsLoggedIn)
+// // console.log("the value of IsLoggedin is"+BooleanIsLoggedIn)
+// // // 1->true
+// // // 0->false
+// // // "" empty string false
+// // // "abc"->true
 // let num=33
-let numToString=String(num)
-console.log(numToString);
-console.log(typeof numToString);
+// let numToString=String(num)
+// console.log(numToString);
+// console.log(typeof numToString);
+
+// // ---------------------------*operations*----------------------------
+
+
+//  let value=3
+//  let negValue=-value
+// //  console.log(negValue);
+
+// let str1 ="adwaya"
+// let str2=" adhikary"
+// let str3=str1+str2
+// console.log(str3);
+// console.log(2+"2");
+// console.log("2"+2);
+// console.log(1+2+"2")//
+// console.log("1"+2+3);//if string is first then every other param is treated as a string but if string is last then others are treated as a numbers but the the last is treated as a number
+
+
+
+
+
+// console.log(+true);
+// console.log(+""); 
+
+let a =3
+let b = a++
+console.log({a}+{b});
+
+let x =3
+let y = ++x
+console.log('x:${x}'+ 'y:${y}');
