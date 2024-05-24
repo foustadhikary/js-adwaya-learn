9dbdc6450fee7d7a431abf3afe7652253a9b570a
foustadhikary <134725133+foustadhikary@users.noreply.github.com>
2024-05-24 12:38:44 +0000

    config changes made to numbers

---
+++ js/07_numbers.js
@@ -0,0 +1,50 @@
+// console.log("adwaya")
+const score=400
+// console.log(score);
+const balance=new Number(100)
+
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2))// after decimal 2 values
+
+const otherNumber= 213.9928
+// console.log(otherNumber.toPrecision(3));// focus on first 3 value and make it approx if nth digit are there we will get value for n+1 th digit
+// console.log(" ");
+// console.log(" ");
+// console.log(" ");
+// console.log(" ");
+// console.log(" ");
+// const hundreds=10000000000
+// // console.log(hundreds.toLocaleString('en-IN'));
+
+
+// //++++++++++++++++++++++++++++++maths++++++++++++++++++++++
+// let arr=new Array(6)//here we pass the parameter to find the size of the array
+
+
+
+// arr[0]=1
+// arr[1]=2
+// // arr[0]=1
+// // arr[0]=1
+// // arr[0]=1
+// // arr[0]=1
+// // arr[0]=1
+// // arr[0]=1
+// console.log(arr.length);
+// // console.log(Math);
+// // console.log(Math.abs(-4));
+// // console.log(Math.round(4.8));
+// // console.log(Math.ceil(4.2));
+// // console.log(Math.floor(4.2));
+// console.log(Math.max(4,2,45,52,56,7,8,2,67,-2));
+
+
+
+console.log(Math.random());//always b/w 0-1
+console.log(Math.floor((Math.random()*10) + 1))//1-9
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min);
