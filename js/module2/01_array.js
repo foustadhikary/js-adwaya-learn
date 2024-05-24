025180cf75ab9040f1366dfb94e6f4cf1729a49d
foustadhikary <134725133+foustadhikary@users.noreply.github.com>
2024-05-24 14:20:49 +0000

    config cahnge to all file in documents

---
+++ js/module2/01_array.js
@@ -0,0 +1,11 @@
+//array
+//(always in square braces)
+const myArr=[0, 1, 2, 3, 4, 5 ]//array not necessary have same type of elements
+//javascript array are resizale and have mixed datatypes
+//not associative array
+
+console.log(myArr[4]);
+
+const myArr2=new Array(1,2,3,4,5)
+console.log(myArr2);
+
