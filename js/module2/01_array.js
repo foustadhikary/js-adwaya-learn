056ee53afd5293f56efef4382c51563849419435
foustadhikary <134725133+foustadhikary@users.noreply.github.com>
2024-05-27 05:34:45 +0000

    config changes to array

--- js/module2/01_array.js
+++ js/module2/01_array.js
@@ -4,8 +4,51 @@ const myArr=[0, 1, 2, 3, 4, 5 ]//array not necessary have same type of elements
 //javascript array are resizale and have mixed datatypes
 //not associative array
 
-console.log(myArr[4]);
+// console.log(myArr[4]);
 
 const myArr2=new Array(1,2,3,4,5)
-console.log(myArr2);
+// console.log(myArr2);
+
+//array methods
+
+
+
+// myArr.push(6)
+// console.log(myArr); 
+// myArr.pop()//remove last element
+// myArr.unshift (9)//element is inserted in the starting of array
+// console.log(myArr); 
+// myArr.shift()// remove from starting
+// console.log(myArr); 
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+const newArr=myArr.join()//converts array to string
+// console.log(myArr);
+// console.log(newArr);
+
+
+//slice and splice
+console.log("A",myArr);
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+console.log("");
+console.log("");
+
+console.log("B",myArr);
+console.log("");
+console.log("");
+
+const myn2=myArr.splice(1,3)
+console.log(myn2);
+console.log("original array",myArr);
+
+//  splice vs slice  
+
+
+
+
+
 
