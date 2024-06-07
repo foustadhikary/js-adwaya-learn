6bb0fb709bbe85fafcf1ce13b80922d89e988c01
foustadhikary <134725133+foustadhikary@users.noreply.github.com>
2024-06-07 12:29:13 +0000

    config changes

--- js/module2/scope.js
+++ js/module2/scope.js
@@ -1,13 +1,41 @@
 // var c=300// var is a bekar value
-let a =300
-
-if (true){//block scope
-
-    let a =10;
-    const b=20
-    console.log("inner: ", a);
-   
-}
-console.log(a);// a in global scope is 300 and insode block it is 
+// 
 // console.log(b);
 // console.log(c);
+//clouser: filhal child function parent kaay variable ko access kar pate hai
+
+function one(){
+    const userName="hitest"
+    function two(){
+        const website="youtube"
+        console.log(userName);
+    }
+    // console.log(website);// website ka scope pehle hee khatam ho gya
+    // two()
+
+}
+// one()
+
+// if (true){
+//     const userName="hitesh"
+//     if (userName==="hitesh"){
+//         const website=" youtube"
+//         console.log(userName+ website);
+//     }
+//     // console.log(website);
+// }
+// console.log(userName);
+
+
+// ++++++++++++++++++++++++++++++ intersting++++++++++++++++++++++
+console.log(addOne(5))
+function addOne(num){
+    return num+1
+}
+
+
+addTwo(5)//decleration se pehle access nhi kar payengay if we store function on a variable
+const addTwo= function(num){
+    return num+2
+}
+
