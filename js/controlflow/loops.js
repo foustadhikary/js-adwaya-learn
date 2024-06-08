568263f00f89bb6c705643a9b3ae4fefab22c8e1
foustadhikary <134725133+foustadhikary@users.noreply.github.com>
2024-06-08 12:13:44 +0000

    config changes

--- js/controlflow/loops.js
+++ js/controlflow/loops.js
@@ -1,6 +1,28 @@
-for (let index = 0; index < 10; index++) {
-    const element=index;
+// for (let index = 0; index < 10; index++) {
+//     const element=index;
+//     if (element== 5){
+//         console.log(`${element}, is best number`);
+//     }
+//     console.log(element);
+    
+    
+// }
+
+let myArray=['flash','batman','superman'] 
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
     console.log(element);
     
+}
+//keyword: break and continue
+
+for (let i = 1; i <= 20; i++) {
+    if (i==5){
+        console.log(`detected 5`);
+        continue;
+    }
+    console.log(`value of i is ${i}`);
+
     
 }
