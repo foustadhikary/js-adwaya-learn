b9f429db68647211d839eaa5981f00d36f8ca8af
foustadhikary <134725133+foustadhikary@users.noreply.github.com>
2024-07-17 19:22:45 +0000

    config changes

--- backend/http.js
+++ backend/http.js
@@ -3,8 +3,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// app.get("/route-handler",function(req,res){
+//     //header, body , query-parameter
+//     //do machine learning model
+//     res.json({
+//         name:"adwaya",
+//         age:21
+//     })
+// })
+
 app.get('/', function(req, res) {
-  res.send('Hello World!')
+  res.send('<b> hi there </b>')
 })
 
 app.listen(port, function(){
